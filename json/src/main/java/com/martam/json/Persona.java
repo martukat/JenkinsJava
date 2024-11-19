362d3db274a60b63f95f809375d4a26ed4ea3d68
martam <marta.munoz@altia.es>
2024-11-19 15:13:32 +0100

    Creamos primeras clases con la función de leer un JSON.

--- json/src/main/java/com/martam/json/Persona.java
+++ json/src/main/java/com/martam/json/Persona.java
@@ -8,4 +8,27 @@ package com.martam.json;
  */
 public class Persona {
 
+	private String nombre;
+
+	/**
+	 * @param nombre
+	 */
+	public Persona(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
--- json/src/main/java/com/martam/json/Principal.java
+++ json/src/main/java/com/martam/json/Principal.java
@@ -12,7 +12,13 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String json = "{\"nombre\" : \"Pedro\"}";
+		LeerJsonPersona leer = new LeerJsonPersona();
+		
+		Persona p1 = leer.getPersona(json);
+		
+		System.out.println(p1.getNombre());
 
 	}
 
